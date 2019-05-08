fac677e400a3caf33414029cbc18262826223e4e
waterbeside <waterbeside@163.com>
2019-05-08 12:15:44 +0800

    feat: api for edit sppo

--- src/api/sppo.js
+++ src/api/sppo.js
@@ -1,7 +1,11 @@
 import request from '@/utils/request'
 import settings from '@/settings.js'
-import qs from 'qs'
+// import qs from 'qs'
 
+/**
+ * 取得SPPO列表
+ * @param Object params 
+ */
 export function getList(params) {
   return request({
     url: settings.apiBaseUrl+'/api/v1/sppo',
@@ -10,6 +14,10 @@ export function getList(params) {
   })
 }
 
+/**
+ * 新建Sppo
+ * @param Object data 
+ */
 export function add(data) {
   return request({
     url: settings.apiBaseUrl+'/api/v1/sppo',
@@ -18,6 +26,36 @@ export function add(data) {
   })
 }
 
+/**
+ * 编辑Sppo
+ * @param Object data 
+ */
+export function edit(data) {
+  return request({
+    url: settings.apiBaseUrl+'/api/v1/sppo',
+    method: 'put',
+    data
+  })
+}
+
+/**
+ * 批量编辑Sppo
+ * @param Object data 
+ */
+export function batchEdit(data) {
+  return request({
+    url: settings.apiBaseUrl+'/api/v1/sppo/batch',
+    method: 'put',
+    data
+  })
+}
+
+
+
+/**
+ * 删除
+ * @param Object data 
+ */
 export function del(data) {
   // data = qs.stringify(data) ;
   console.log(data)
@@ -28,7 +66,10 @@ export function del(data) {
   })
 }
 
-
+/**
+ * 检查Customer_Fab_Code是否存在
+ * @param Object params 
+ */
 export function checkCustomerFabCodeExist(params){
   return new Promise((resole,reject)=>{
     request({
