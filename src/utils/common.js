9f859bbfe9e1f9a4fea5c329220e466f29a9700c
waterbeside <waterbeside@163.com>
2019-05-10 10:04:08 +0800

    feat: 加入myCache工具，使用localStorage或sessionStorage缓存数据

--- src/utils/common.js
+++ src/utils/common.js
@@ -16,11 +16,63 @@ export async function asyncForEach(arr, callback){
     let k = 0;
     while (k < length) {
       if (k in O) {
-        console.log('doing foreach...');
         const kValue = O[k];
         await callback(kValue, k, O);
       }
       k++;
     }
   };
+
+
+  export let myCache = {
+    get:(key,s=0)=>{
+      let storageObj = s === 's' ? window.sessionStorage : window.localStorage ;
+      if(!storageObj){
+        return null;
+      }
+      let res = storageObj.getItem(key)
+      let now = (new Date()).valueOf();
+      if(res){
+          let data = JSON.parse(res);
+          if(typeof(data.exp)=='undefined'){
+            return data;
+          }
+          if(data.exp === 0 || parseInt(data.exp) > (new Date()).valueOf() ){
+            return typeof(data.data)!='undefined' ? data.data : null;
+          }
+          return null
+      }else{
+        return null;
+      }
+    },
+
+    set:(key,value,exp=0)=>{
+      let storageObj = exp === 's' ? window.sessionStorage : window.localStorage ;
+      if(!storageObj){
+        return null;
+      }
+      
+      let now = (new Date()).valueOf();
+      let setData = {
+        exp:exp === 0 ? 0 : exp*1000 + now,
+        data:value
+      }
+      let dataString = JSON.stringify(setData);
+      storageObj.setItem(key,dataString);
+      return;
+    },
+
+    do:async(key,[fun,data=null],exp=0)=>{
+      let res = myCache.get(key,exp);
+      if(res === null){
+        if(typeof(fun)=='function'){
+          res = await fun(data);
+        }
+        myCache.set(key,res,exp);
+      }else{
+        // console.log('mycache:'+key)
+      }
+      return res;
+    }
+  }
 
