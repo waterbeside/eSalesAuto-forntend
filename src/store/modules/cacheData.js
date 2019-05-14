534dde8c94fa2f4cc38b94576940949e53a9f23e
waterbeside <waterbeside@163.com>
2019-05-14 10:46:51 +0800

    feat: getFactorys action

--- src/store/modules/cacheData.js
+++ src/store/modules/cacheData.js
@@ -1,19 +1,47 @@
+import {assistAPI} from '@/api'
 const cacheData = {
   namespaced: true,
 
   state: {
-    factorys: []
+    factorys: [],
+    factorys_go: [],
   },
   mutations: {
     SET_FACTORYS: (state, data) => {
       state.factorys = data
+    },
+    SET_FACTORYS_GO: (state, data) => {
+      state.factorys_go = data
     },
     SET_WASH_TYPES: (state, data) => {
       state.washTypes = data
     }
   },
   actions: {
-    
+    getFactorys({ commit, state },type){
+      return new Promise((resolve, reject) => {
+        type = type || 0;
+        let cacheList = type ? state.factorys_go : state.factorys;
+        if(cacheList && cacheList.length > 0){
+          resolve(cacheList);
+          return ;
+        }
+        assistAPI.getFactoryIds({type}).then((res)=>{
+          let list = res.data.list;
+          if(type){
+            commit('SET_FACTORYS_GO', list)
+          }else{
+            commit('SET_FACTORYS', list)
+          }
+          resolve(list);
+          return ;
+        }).catch(error=>{
+          reject(error)
+        })
+      }).catch(error => {
+        reject(error)
+      })
+    }
   }
 }
 
