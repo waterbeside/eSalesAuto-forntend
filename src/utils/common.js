06f7da5bc6adb6118578f2df48057af272054988
waterbeside <waterbeside@163.com>
2019-05-14 10:45:59 +0800

    feat: 添加格式化“通过Excel导入日期”的方法

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,3 +1,10 @@
+import moment from 'moment'
+
+/**
+ * 把json的key转为大写或小写
+ * @param {Object} jsonObj 
+ * @param {Integer} type 0:转小写,1:转大写
+ */
 export function changeCaseJsonKey(jsonObj,type = 0){
     for (var key in jsonObj){
         let nkey =  type ? key.toUpperCase() : key.toLowerCase();
@@ -10,69 +17,90 @@ export function changeCaseJsonKey(jsonObj,type = 0){
 }
 
 
+/**
+ * 支持async-await的ForEach方法
+ * @param {Array} arr 
+ * @param {Function} callback 
+ */
 export async function asyncForEach(arr, callback){
-    const length = arr.length;
-    const O = Object(arr);
-    let k = 0;
-    while (k < length) {
-      if (k in O) {
-        const kValue = O[k];
-        await callback(kValue, k, O);
-      }
-      k++;
+  const length = arr.length;
+  const O = Object(arr);
+  let k = 0;
+  while (k < length) {
+    if (k in O) {
+      const kValue = O[k];
+      await callback(kValue, k, O);
     }
-  };
-
-
-  export let myCache = {
-    get:(key,s=0)=>{
-      let storageObj = s === 's' ? window.sessionStorage : window.localStorage ;
-      if(!storageObj){
-        return null;
-      }
-      let res = storageObj.getItem(key)
-      let now = (new Date()).valueOf();
-      if(res){
-          let data = JSON.parse(res);
-          if(typeof(data.exp)=='undefined'){
-            return data;
-          }
-          if(data.exp === 0 || parseInt(data.exp) > (new Date()).valueOf() ){
-            return typeof(data.data)!='undefined' ? data.data : null;
-          }
-          return null
-      }else{
-        return null;
-      }
-    },
-
-    set:(key,value,exp=0)=>{
-      let storageObj = exp === 's' ? window.sessionStorage : window.localStorage ;
-      if(!storageObj){
-        return null;
-      }
-      
-      let now = (new Date()).valueOf();
-      let setData = {
-        exp:exp === 0 ? 0 : exp*1000 + now,
-        data:value
-      }
-      let dataString = JSON.stringify(setData);
-      storageObj.setItem(key,dataString);
-      return;
-    },
-
-    do:async(key,[fun,data=null],exp=0)=>{
-      let res = myCache.get(key,exp);
-      if(res === null){
-        if(typeof(fun)=='function'){
-          res = await fun(data);
-        }
-        myCache.set(key,res,exp);
-      }else{
-        // console.log('mycache:'+key)
-      }
-      return res;
-    }
+    k++;
   }
+};
+
+
+/**
+ * 格式化从excel导入的日期字段
+ * @param {Integer} numb  从EXCEL导入的日期字段所生成的整数(距1900-01-01的天数)
+ * @param {String} format  格式化后的分隔符
+ */
+export function formatDateFromExcel(numb, format='YYYY-MM-DD') {
+  // let time = new Date((parseInt(numb) - 1) * 24 * 3600000 + 1)
+  // time.setYear(time.getFullYear() - 70)
+  let time = new Date(1900, 0, numb - 1);
+  return moment(time).format(format)
+}
+
+
+/**
+ * myCache工具
+ */
+export let myCache = {
+  get:(key,s=0)=>{
+    let storageObj = s === 's' ? window.sessionStorage : window.localStorage ;
+    if(!storageObj){
+      return null;
+    }
+    let res = storageObj.getItem(key)
+    let now = (new Date()).valueOf();
+    if(res){
+        let data = JSON.parse(res);
+        if(typeof(data.exp)=='undefined'){
+          return data;
+        }
+        if(data.exp === 0 || parseInt(data.exp) > (new Date()).valueOf() ){
+          return typeof(data.data)!='undefined' ? data.data : null;
+        }
+        return null
+    }else{
+      return null;
+    }
+  },
+
+  set:(key,value,exp=0)=>{
+    let storageObj = exp === 's' ? window.sessionStorage : window.localStorage ;
+    if(!storageObj){
+      return null;
+    }
+    
+    let now = (new Date()).valueOf();
+    let setData = {
+      exp:exp === 0 ? 0 : exp*1000 + now,
+      data:value
+    }
+    let dataString = JSON.stringify(setData);
+    storageObj.setItem(key,dataString);
+    return;
+  },
+
+  do:async(key,[fun,data=null],exp=0)=>{
+    let res = myCache.get(key,exp);
+    if(res === null){
+      if(typeof(fun)=='function'){
+        res = await fun(data);
+      }
+      myCache.set(key,res,exp);
+    }else{
+      // console.log('mycache:'+key)
+    }
+    return res;
+  }
+}
 
