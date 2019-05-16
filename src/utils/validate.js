d2438b2048d9474bb91103537a1f53498ab2e5f9
waterbeside <waterbeside@163.com>
2019-05-16 09:33:10 +0800

    feat: 添加GO的Combo格式验证

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -72,6 +72,42 @@ export function checkColorCombo(str,callback){
 }
 
 /**
+ * 检证AutoGO combo格式是否正确
+ */
+export function checkGoCombo(combo,callback){
+  let code = 0;
+  let msg = '';
+  if(_.trim(combo.substring(2,3)) != ''){
+    msg = "Combo 第三位必须为空";
+    code = 1;
+  }
+  if(combo.length < 4){
+    msg = 'Combo 格式不正确';
+    code = 1;
+  }else{
+    let combo_front = combo.substring(0,2);
+    let combo_tempArray = combo.split('-');
+    if(typeof(combo_tempArray[1])=="undefined"){
+      errorMsg[index].combo = 'Combo 格式不正确';
+      row.error.push('combo');
+      hasError = true;
+    }else{
+      let combo_end   = combo_tempArray[1];
+      if(parseInt(combo_front) != parseInt(combo_end) || parseInt(combo_front) < 1 ){
+        msg = 'Combo 格式不正确';
+        code = 1;
+      }
+    }
+  }
+
+  
+  if(typeof(callback)=='function') callback({code,msg});
+  return code !== 0 ? false : true;
+
+}
+
+
+/**
  * 检证collar_cuff_size格式是否正确
  */
 export function checkCollarCuffSize(str,callback){
@@ -104,6 +140,27 @@ export function checkDelivery(obj){
   let now = moment(moment().format('YYYY-MM-DD')).unix();
   let checkDate = moment(obj).unix();
   return checkDate > now ;
+}
+
+/**
+ * 检查列表中，相同的某一个字段，其它所有另一个字段必须相同
+ * @param {Object} row  当前行
+ * @param {Object} list 当前列表
+ * @param {Array} fields 要查的字段
+ */
+export function checkMustSame(row,list,fields){
+  let code = 0;
+  let msg = '';
+  console.log(fields);
+  let otherdRowIndex = list.findIndex(item=>{
+    return (_.trim(item[fields[0]]) == _.trim(row[fields[0]]) && _.trim(item[fields[1]]) != _.trim(row[fields[1]]) &&  item.id != row.id);
+  })
+  if(otherdRowIndex != -1){
+    code = 1;
+    return otherdRowIndex;
+  }else{
+    return -1;
+  }
 }
 
 
