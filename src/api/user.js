9442d971273b648c1f38393b17cb705950dafa41
waterbeside <waterbeside@163.com>
2019-05-22 17:43:35 +0800

    feat: 用户管理相关接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,10 @@
 import request from '@/utils/request'
 import settings from '@/settings.js'
 
+/**
+ * 登入
+ * @param {Object} data 
+ */
 export function login(data) {
   return request({
     url: settings.apiBaseUrl+'/api/v1/passport/login',
@@ -9,6 +13,9 @@ export function login(data) {
   })
 }
 
+/**
+ * 取得当前登入用户的信息
+ */
 export function getInfo() {
   return request({
     url: settings.apiBaseUrl+'/api/v1/passport',
@@ -16,6 +23,9 @@ export function getInfo() {
   })
 }
 
+/**
+ * 登出
+ */
 export function logout() {
   return request({
     url: settings.apiBaseUrl+'/api/v1/passport',
@@ -23,4 +33,59 @@ export function logout() {
   })
 }
 
+/**
+ * 取得用户列表
+ */
+export function getList(params) {
+  return request({
+    url: settings.apiBaseUrl+'/api/v1/user',
+    method: 'get',
+    params
+  })
+}
 
+/**
+ * 取得用户详情
+ */
+export function userDetail(id,params) {
+  return request({
+    url: settings.apiBaseUrl+'/api/v1/user/'+id,
+    method: 'get',
+    params
+  })
+}
+
+/**
+ * 取得用户详情
+ */
+export function userUpdate(id,data) {
+  return request({
+    url: settings.apiBaseUrl+'/api/v1/user/'+id,
+    method: 'put',
+    data
+  })
+}
+
+/**
+ * 新增用户
+ */
+export function userAdd(data) {
+  return request({
+    url: settings.apiBaseUrl+'/api/v1/user',
+    method: 'post',
+    data
+  })
+}
+
+
+
+/**
+ * 删除用户
+ */
+export function userDelete(data) {
+  return request({
+    url: settings.apiBaseUrl+'/api/v1/user',
+    method: 'delete',
+    data
+  })
+}
