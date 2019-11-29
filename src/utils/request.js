86b0f424a272b29c6283892a66edaa1716897c2b
waterbeside <waterbeside@163.com>
2019-11-29 16:15:24 +0800

    other: 延长请求超时时间

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,7 +7,7 @@ import { getToken } from '@/utils/auth'
 const service = axios.create({
   // baseURL: process.env.VUE_APP_BASE_API, // api 的 base_url
   // withCredentials: true, // 跨域请求时发送 cookies
-  timeout: 60*1000 // request timeout
+  timeout: 60*1000 * 3// request timeout
 })
 
 // request interceptor
